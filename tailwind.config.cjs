2606f339ef4c1c78321dff784030ccb64d69613d
Diego Coscolla <56701155+dcos96038@users.noreply.github.com>
2022-10-31 20:13:39 -0300

    Project config

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    colors: {
+      background: "#0D0D0E",
+      primary: "#AF06FF",
+      secondary: "#6909F5",
+      accent: "#F234CB",
+      white: "#FFFFFF",
+    },
+  },
+  plugins: [],
+};
