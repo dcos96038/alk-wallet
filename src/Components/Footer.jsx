da5c5cda666a43f8a185d88d969dbdd49fb3bd8e
Ramiro Guillen <94654482+ramiroguillen@users.noreply.github.com>
2022-11-01 17:17:56 -0300

    Footer with year and who developed the website (#3)

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,3 +1,12 @@
 export function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className="w-full flex items-center">
+      <p className="mx-auto">
+        {"Copyright ⓒ " + new Date().getFullYear() + " - "}
+        <a href="#" className="no-underline hover:text-primary">
+          Grupo R-2 SKU React - Alkemy
+        </a>
+      </p>
+    </footer>
+  );
 }
