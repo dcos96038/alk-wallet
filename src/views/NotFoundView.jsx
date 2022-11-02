cd00f1add91e854a99bd0d726f5aeb9b243c9b85
ramiroguillen <ramirojguillen@gmail.com>
2022-11-02 18:39:38 -0300

    not found page view

--- src/views/NotFoundView.jsx
+++ src/views/NotFoundView.jsx
@@ -1,10 +1,17 @@
+import {Link} from "react-router-dom";
 
+import Button from "../Components/Button";
 
 const NotFoundView = () => {
   return (
-    <div className="flex flex-column flex-grow justify-center items-center">
-      <h2>404</h2>
-      <h3>Página no encontrada</h3>
+    <div className="flex flex-col flex-grow justify-center items-center">
+      <h2 className="text-primary text-8xl">404</h2>
+      <h3 className="text-2xl">Página no encontrada</h3>
+      <Button className="mt-6" variant="primary">
+        <Link className="text-white no-underline" to="/home">
+          Volver a Home
+        </Link>
+      </Button>
     </div>
   );
 };
