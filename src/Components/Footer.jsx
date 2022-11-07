c11845b6da25218e19a5c5bffd04a77dd86a162f
ValentinMarturet <87504643+ValentinMarturet@users.noreply.github.com>
2022-11-07 20:30:45 -0300

    Valen/alk 31 skeleton (#10)
    * flex-grow for views
    
    * added red and green colors
    
    * not found page view
    
    * view and components for balance page
    
    * responsiveness for balance view
    
    * Creando skeleton para la vista de Charge
    Agregando componente Dropdown
    
    * Agregando estructura de la vista SendView
    Cambiando detalles de la vista ChargeView
    Cambiando detalles del componente Dropdown
    
    * Expenses view
    
    Co-authored-by: ramiroguillen <ramirojguillen@gmail.com>
    Co-authored-by: Telayna-I <telayna98@gmail.com>

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,9 +1,9 @@
 export function Footer() {
   return (
-    <footer className="w-full flex items-center">
+    <footer className="w-full flex items-center h-10">
       <p className="mx-auto">
         {"Copyright ⓒ " + new Date().getFullYear() + " - "}
-        <a href="#" className="no-underline hover:text-primary">
+        <a className="no-underline text-primary hover:text-accent" href="#">
           Grupo R-2 SKU React - Alkemy
         </a>
       </p>
