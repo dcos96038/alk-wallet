b6f5a7afa83b8132ecc3643cc3c98054ead1f623
Matias <matias.pz@proton.me>
2022-11-07 20:42:51 -0300

    Matiaspz/alk 26 movimientos (#7)
    * login working
    
    * buttons don't float right by default anymore
    
    * added movements view
    
    * fixed login not updating state until next refresh
    
    Co-authored-by: Ramiro Guillen <94654482+ramiroguillen@users.noreply.github.com>

--- src/views/MovementsView.jsx
+++ src/views/MovementsView.jsx
@@ -1,9 +1,87 @@
-import React from 'react'
+import {useCallback, useEffect, useState} from "react";
+import {useSelector} from "react-redux";
+
+import {walletApi} from "../api";
+import {selectUser} from "../redux/userSlice";
+import Button from "../Components/Button";
+import Loader from "../Components/Loader";
 
 const MovementsView = () => {
-  return (
-    <div>MovementsView</div>
-  )
-}
+  const {accessToken} = useSelector(selectUser);
+  const [transactions, setTransactions] = useState();
+  const [error, setError] = useState();
+  const [prevPage, setPrevPage] = useState();
+  const [nextPage, setNextPage] = useState();
 
-export default MovementsView
+  const loadPage = useCallback(
+    async (pageUrl) => {
+      setPrevPage();
+      setNextPage();
+      setTransactions();
+      try {
+        const {
+          data: {previousPage, nextPage, data},
+        } = await walletApi.get(pageUrl, {
+          headers: {Authorization: "Bearer " + accessToken},
+        });
+
+        setPrevPage(previousPage);
+        setNextPage(nextPage);
+        setTransactions(data);
+      } catch (e) {
+        setError(e.response?.data?.error || e.message);
+
+        return;
+      }
+    },
+    [accessToken],
+  );
+
+  useEffect(() => {
+    loadPage("/transactions");
+  }, [loadPage]);
+
+  return (
+    <div className="p-5">
+      <h1 className="text-primary">Movements</h1>
+      {error ? (
+        <div className="mx-4 my-10 text-xl font-bold">Error: {error}</div>
+      ) : transactions ? (
+        <div className="my-10">
+          <table className="w-full">
+            <thead>
+              <tr>
+                <th>Type</th>
+                <th>Concept</th>
+                <th>Amount</th>
+                <th>To account</th>
+                <th>Date</th>
+              </tr>
+            </thead>
+            <tbody>
+              {transactions.map((tx) => (
+                <tr key={tx.id} className="odd:bg-black/[0.05] odd:dark:bg-white/[0.05]">
+                  <td className="text-center">{tx.type}</td>
+                  <td className="text-center">{tx.concept}</td>
+                  <td className="text-center">{tx.amount}</td>
+                  <td className="text-center"> {tx.to_account_id}</td>
+                  <td className="text-center">{new Date(tx.date).toLocaleString()} </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <div className="mt-10 flex justify-center gap-2">
+            {prevPage && <Button action={() => loadPage(prevPage)}>{"<"}</Button>}
+            {nextPage && <Button action={() => loadPage(nextPage)}>{">"}</Button>}
+          </div>
+        </div>
+      ) : (
+        <div className="mx-4 my-10 grid place-content-center">
+          <Loader />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default MovementsView;
