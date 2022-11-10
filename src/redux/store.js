fd299a26af9472283dbe119c3233f3c0f3ccbbd9
unknown <valen.martu2206@gmail.com>
2022-11-10 20:41:13 -0300

    Agregando logica para la view sendView Se creo el slice accountSlice para manejar la logica de la cuenta Se agrego el accountReducer al store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
-import {configureStore} from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 
-import {userReducer} from "./userSlice";
+import { userReducer } from "./userSlice";
+import { accountReducer } from "./accountSlice";
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
+    account: accountReducer,
   },
 });
